8fc213c0b482f5f96e378dceb98a957e932d77bb
Hakan Sezer <62359186+HakanSezer@users.noreply.github.com>
2023-10-14 16:57:35 +0300

    Add files via upload
    Bu ödev üzerinde identifier kullanmadan sayfalar arası butonlarla nasıl geçiş yapılır bunları görmüş oldum.

---
+++ odev 4- sayfa gecis/odev 4- sayfa gecis/CıkısViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CıkısViewController.swift
+//  odev 4- sayfa gecis
+//
+//  Created by Hakan Sezer on 14.10.2023.
+//
+
+import UIKit
+
+class C_k_sViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ odev 4- sayfa gecis/odev 4- sayfa gecis/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  odev 4- sayfa gecis
+//
+//  Created by Hakan Sezer on 14.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
